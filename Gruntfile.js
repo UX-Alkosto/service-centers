2e01303e8cbf5c39c13ce787b625ec35e61cdf54
Miguel Colmenares <me@miguelcolmenares.com>
2021-02-07 14:07:58 -0500

    Css min
    Added css-min task
    Refactored grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,34 @@
 'use strict';
 module.exports = function (grunt) {
 
+    var themes = ['kalley']
+
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
         clean: {
             dist: ["dist/**/css/*", "dist/**/js/*", "dist/**/fonts/*"]
+        },
+        cssmin: {
+            options: {
+                banner: "/*! <%= pkg.name %> - v<%= pkg.version %> */",
+                compatibility: 'ie8',
+                report: 'gzip',
+                inline: ['local'],
+                level: {
+                    1: {
+                        all: true
+                    },
+                    2: {
+                        all: true
+                    }
+                }
+            },
+            dist: {
+                expand: true,
+                cwd: 'dist',
+                src: ['**/css/*.css'],
+                dest: 'dist'
+            }
         },
         copy: {
             fonts: {
@@ -24,22 +48,21 @@ module.exports = function (grunt) {
                 banner: "/*! <%= pkg.name %> - v<%= pkg.version %> */",
                 compress: true,
                 paths: ['dist/css'],
+                modifyVars: {
+                    themeName: '<%= theme %>'
+                },
                 plugins: [
                     new (require('less-plugin-autoprefix'))({ browsers: ["last 2 versions"] }),
                     new (require('less-plugin-clean-css'))({ advanced: true })
                 ]
             },
-            kalley: {
-                options: {
-                    modifyVars: {
-                        themeName: 'kalley'
-                    }
-                },
+            theme: {
                 files: {
-                    'dist/kalley/css/servicio.css': 'src/less/servicio.less'
+                    'dist/<%= theme %>/css/servicio.css': 'src/less/servicio.less'
                 }
             }
         },
+        themes: themes,
         uglify: {
             options: {
                 banner: "/*! <%= pkg.name %> - v<%= pkg.version %> */",
@@ -62,8 +85,16 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-uglify-es');
 
-    grunt.registerTask('default', ['clean', 'copy', 'less', 'uglify']);
+    grunt.registerMultiTask('themes', 'Generate styles for each site', function () {
+        const done = this.async();
+        grunt.log.writeln('Compile less for: ' + this.data);
+        grunt.config('theme', this.data);
+        grunt.task.run('less');
+        done();
+    });
+    grunt.registerTask('default', ['clean', 'copy', 'themes', 'cssmin', 'uglify']);
 };
