6ac22c6d682e0e8428e19abe1742528f55742244
Miguel Colmenares <me@miguelcolmenares.com>
2021-02-14 01:13:33 -0500

    🔖 v1.0.5

--- dist/common/js/service-center.js
+++ dist/common/js/service-center.js
@@ -1,2 +1,2 @@
-/*! service-centers - v1.0.4 */
+/*! service-centers - v1.0.5 */
 export default class ServiceCenter{constructor({address:e="",areaCode:s="",cell:t=[],coordinates:i={},hours:h=[],id:a="",name:r="",phone:o=[],viewMap:c=""}){this.address=e,this.areaCode=s,this.cellphone=t,this.coordinates=i,this.hours=h,this.id=a,this.isActive=!1,this.name=r,this.phone=o,this.map=c}get active(){return this.isActive}set active(e){this.isActive=e}};
