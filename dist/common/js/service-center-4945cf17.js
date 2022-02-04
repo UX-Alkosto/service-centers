06d36615a10dc9df3e62017b689bf82293695d66
alejaCrea20 <61421003+alejaCrea20@users.noreply.github.com>
2022-02-04 16:44:24 -0500

    Actualizacion CSA para kalley
    Se borró el teléfono fijo del CSA valle-cauca-jamundi para Kalley.

--- dist/common/js/service-center-4945cf17.js
+++ dist/common/js/service-center-4945cf17.js
@@ -1,2 +1,2 @@
-/*! service-centers - release: 1.3.9 */
+/*! service-centers - release: 1.4.0 */
 class i{constructor({address:i="",areaCode:t="",cell:e=[],city:s="",coordinates:h={},email:a="",hours:c=[],id:l="",link:n="",type:r="",name:o="",phone:d=[],schedule:p=[],viewMap:m=""}){this.address=i,this.areaCode=t,this.cellphone=e,this.city=s,this.coordinates=h,this.email=a,this.hours=c,this.id=l,this.isActive=!1,this.link=n,this.linkType=r,this.name=o,this.phone=d,this.schedule=p,this.map=m}get active(){return this.isActive}set active(i){this.isActive=i}get isCallCenter(){return!(!this.city.match("linea-nacional")&&!this.id.match("call-center"))}}export{i as ServiceCenter};
